2da1687cf6025fb09fad7384e1ac777fb3b3702c
Gerardo Delgado <gdelgado@adobe.com>
2018-12-06 09:29:30 -0500

    added another cluster

---
+++ compositions/qe/main.tf
@@ -0,0 +1,14 @@
+variable "profile" {}
+variable "shared_credentials_file" {}
+variable "environment" {}
+variable "account_id" {}
+
+module "common_main_resources-va6" {
+  source = "../../modules/example"
+
+  profile                 = "${var.profile}"
+  shared_credentials_file = "${var.shared_credentials_file}"
+  environment             = "${var.environment}"
+  region                  = "us-east-1"
+  account_id              = "${var.account_id}"
+}
---
+++ modules/qe/variables.tf
@@ -0,0 +1,5 @@
+variable "profile" {}
+variable "region" {}
+variable "environment" {}
+variable "account_id" {}
+variable "shared_credentials_file" {}

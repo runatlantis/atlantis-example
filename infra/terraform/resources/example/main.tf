48132b004c9d21539c7954094c2669b62e56918c
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2018-12-20 12:31:50 +0000

    Add S3 state backend

--- infra/terraform/resources/example/main.tf
+++ infra/terraform/resources/example/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket               = "mojap-atlantis-terraform-test"
+    workspace_key_prefix = "example:"
+    key                  = "terraform.tfstate"
+    region               = "eu-west-1"
+  }
+}
+
 provider "aws" {
   region  = "eu-west-1"
   version = "~> 1.50"

82feddbf8f2a1233460d70203f5e47ec273ccc57
JackaDu <jack.du@doordash.com>
2019-03-07 17:57:14 -0800

    test

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-variable "environment_list" {
-  description = "Environment ID"
-  type = "list"
-  default = ["dev", "qa", "prod"]
-}
-variable "env" {
-  default = "test"
-  description = "Which environment do you want (options: dev, qa, prod):"
-}
 resource "null_resource" "is_environment_name_valid" {
-  count = "${contains(var.environment_list, var.env) == true ? 0 : 1}"
-  "ERROR: The env value can only be: dev, qa or prod" = true
+  depends_on = [ "${data.aws_route_table.priv}" ]
 }

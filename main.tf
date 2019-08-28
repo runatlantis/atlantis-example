57572f5ce1aa8673c166e5bac99ae2fc027f948e
Roman Voroncov <romanvoroncov@gmail.com>
2019-08-28 12:40:22 +0300

    add random

--- main.tf
+++ main.tf
@@ -3,3 +3,12 @@ resource "null_resource" "example" {
 
 
 provider "consul" {}
+
+
+resource "random_id" "server" {
+  keepers = {
+    # Generate a new id each time we switch to a new AMI id
+    ami_id = "${var.ami_id}"
+  }
+  byte_length = 8
+}

b01518d2d50fdf3527b0d00f73062c585cd9948f
ethicalmohit <mail@imohit.me>
2019-11-13 12:49:07 +0530

    Adding a resource.

--- main.tf
+++ main.tf
@@ -1,2 +1,6 @@
-resource "null_resource" "example" {
+#resource "null_resource" "example" {
+#}
+resource "aws_ecs_task_definition" "service" {
+  family                = "service"
+  container_definitions = "${file("task-definitions/service.json")}"
 }

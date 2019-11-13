2f9c63b59680ef4216e5b86f0a80c4ad109111c9
ethicalmohit <mail@imohit.me>
2019-11-13 12:50:24 +0530

    New branch with a resource.

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 #resource "null_resource" "example" {
 #}
+
 resource "aws_ecs_task_definition" "service" {
   family                = "service"
   container_definitions = "${file("task-definitions/service.json")}"

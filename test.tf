a9fa372483c4c72b6b6e2f05a79e7a088ae90a9a
Asaf David <asaf.david@allcloud.io>
2019-12-12 23:03:01 +0200

    revert chagnes.

--- test.tf
+++ test.tf
@@ -1,1 +1,16 @@
 resource "null_resource" "example" {}
+
+provider "aws" {
+    version = "~> 2.0"
+    region  = "eu-central-1"
+    profile = "asaf-aws-sandbox"
+}
+
+terraform {
+    beckend "s3" {
+        bucket = "asaf-vault-acct-tfstate"
+        key    = "test-atlantis.tfstate"
+        region = "eu-central-1"
+        profile = "asaf-aws-sandbox"
+    }
+}

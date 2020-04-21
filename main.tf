7b9c6681e187be27f77ac5e4dc2142f356459b07
wlin-jin <1165492051@qq.com>
2020-04-21 19:26:17 +0800

    fix

--- main.tf
+++ main.tf
@@ -12,3 +12,8 @@ resource "template_dir" "config2" {
   destination_dir = "/root/demo/instance_config2-test2"
 }
 
+resource "template_dir" "config3" {
+  source_dir      = "/root/demo/instance_config_templates"
+  destination_dir = "/root/demo/instance_config2-test3"
+}
+

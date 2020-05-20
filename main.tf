d4fb965ac9e9511a75b3e3233caeafcb9fd6d161
Stefhen Hovland <stefhen.hovland@gmail.com>
2020-05-19 21:23:15 -0500

    ami

--- main.tf
+++ main.tf
@@ -1,2 +1,15 @@
-resource "null_resource" "example" {
+provider "aws" {
+  version = "~> 2.22"
+  region = "us-east-1"
+  profile = "sandbox"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners = ["099720109477"]
+  name_regex = "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server*"
+}
+
+output "ami" {
+  value = data.aws_ami.ubuntu.id
 }

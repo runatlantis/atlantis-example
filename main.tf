253260237656366dc53cf2b6f95e3c639094d6d3
codecommer <67277048+codecommer@users.noreply.github.com>
2020-06-30 11:39:46 +0530

    update main.tf
    atlantis plan

--- main.tf
+++ main.tf
@@ -1,2 +1,9 @@
-resource "null_resource" "example" {
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-2332"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }

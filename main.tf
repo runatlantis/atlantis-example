4117a97bc3f8828684e1c0cdd41c5801baee19c0
Leandro Basilio <lmbleandro2010@gmail.com>
2021-12-01 09:18:28 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
-resource "null_resource" "example" {
+resource "aws_s3_bucket" "b" {
+  bucket = "lmb-my-tf-test-bucket"
+  acl    = "private"
 
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }

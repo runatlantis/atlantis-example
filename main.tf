f82559f2607a055049430f1f6e7a47d0ab943626
Leandro Basilio <leandro.basilio@cortex-intelligence.com>
2021-12-06 13:27:05 -0300

    test infracost featuring

--- main.tf
+++ main.tf
@@ -24,8 +24,9 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
 
-  name = "single-instance"
-  instance_count = 3
+  for_each = toset(["one", "two", "three"])
+
+  name = "instance-${each.key}"
 
   ami                    = "ami-ebd02392"
   instance_type          = "t3a.micro"

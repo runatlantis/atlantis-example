2e75d849e2e22cf32cfbbfeb44973af16549988e
Leandro Basilio <leandro.basilio@cortex-intelligence.com>
2021-12-06 13:23:28 -0300

    test infracost featuring

--- main.tf
+++ main.tf
@@ -19,3 +19,23 @@ resource "aws_s3_bucket" "b" {
     Environment = "Dev"
   }
 }
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "single-instance"
+  instance_count = 3
+
+  ami                    = "ami-ebd02392"
+  instance_type          = "t3a.micro"
+  key_name               = "cortex-operacoes"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-039e93481645626c7"]
+  subnet_id              = "subnet-09ad30a977b2366cf"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}

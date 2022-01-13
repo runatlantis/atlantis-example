72e812d4f48c5afdb6bfd19b4cfb9a8ad41e146c
SZA <szaforward2@gmail.com>
2022-01-13 10:14:56 -0600

    add another instance

--- main.tf
+++ main.tf
@@ -26,3 +26,12 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.medium"
+
+  tags = {
+    Name = "HelloWorld2"
+  }
+}

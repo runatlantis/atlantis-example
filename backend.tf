55a9f6dd0241e1a2030b602f09988dc8c80d7177
SZA <szaforward2@gmail.com>
2022-01-13 10:01:32 -0600

    remove backend

--- backend.tf
+++ backend.tf
@@ -1,18 +1,9 @@
 resource "aws_s3_bucket" "tf_course" {
-    
     bucket = "hella-buckets-21321312312"
     acl = "private"
 }
 
-terraform {
-  backend "s3" {
-    encrypt = true    
-    bucket = "hella-buckets-21321312312"
-    dynamodb_table = "terraform-state-lock-dynamo"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+
 
 resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
   name = "terraform-state-lock-dynamo"

001a14676674abd535152730e5ff2dde92d7bb53
yyseik <yuyan.seik@gmail.com>
2022-06-13 11:03:51 +0800

    add bucket

--- main.tf
+++ main.tf
@@ -11,3 +11,15 @@ resource "null_resource" "example3" {
 resource "null_resource" "example4" {
 }
 
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.18.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}

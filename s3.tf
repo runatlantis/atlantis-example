94796eb4b9f9ae9cea6171baec85b32a6ec9ac83
yyseik <yuyan.seik@gmail.com>
2022-06-15 14:15:27 +0800

    demo15jun

--- s3.tf
+++ s3.tf
@@ -1,14 +1,5 @@
-# resource "aws_s3_bucket" "b" {
-#   bucket = "yan-tf-test-bucket-1"
-
-#   tags = {
-#     Name        = "My bucket"
-#     Environment = "Dev"
-#   }
-# }
-
-resource "aws_s3_bucket" "d" {
-  bucket = "yan-tf-test-bucket-2"
+resource "aws_s3_bucket" "b" {
+  bucket = "yan-tf-test-bucket-1"
 
   tags = {
     Name        = "My bucket"

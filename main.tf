d00ef2808e0436cca3d9ab4948e63089c74b2e6a
Colin Hobday <colin.hobday@carro.co>
2022-08-23 14:45:14 +0800

    test

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "null_resource" "example" {}
+

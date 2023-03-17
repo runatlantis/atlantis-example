9803369482ed2b47f956b15417d21a737ed930d3
Maksym Prokopov <mprokopov@gmail.com>
2023-03-17 08:33:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,9 @@ resource "null_resource" "example" {
 
 resource "null_resource" "example2" {
 }
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}

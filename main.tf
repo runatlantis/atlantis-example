a0f00331eae8b1eec36f8d74c31615bda4de7a52
pragathithangaraju <pragathi.thangaraju@gmail.com>
2023-08-18 15:22:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 resource "null_resource" "example" {
 }
 
+resource "aws_instance" "example_server" {
+  ami           = "ami-04e914639d0cca79a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Example"
+  }
+}
